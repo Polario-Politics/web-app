938066a467ee8117cf9d92ccac9f60f395647b1b
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-06-29 18:39:21 -0500

    Update update-common script location

--- common/index.d.ts
+++ common/index.d.ts
@@ -5,3 +5,15 @@ export interface User {
     name: string;
 }
 
+/**
+ * Interface for Website Source
+ */
+export interface Source {
+    name: string;
+    displayName: string;
+    url: string;
+    bias: string;
+    accuracy: string;
+    mbfcUrl: string;
+    verifiedDate: Date;
+}
