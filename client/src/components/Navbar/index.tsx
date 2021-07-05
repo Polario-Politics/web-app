febc3f9fb1885028a89a8677242fa18929f87640
Nick King <nrking0@gmail.com>
2021-07-05 18:31:49 -0500

    Made Navbar Stickey

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,19 +1,21 @@
 import React, { ReactElement } from 'react';
-import { Header, Image } from 'semantic-ui-react';
+import { Header, Image, Sticky } from 'semantic-ui-react';
 
 import './styles.scss';
 
 const Navbar = (): ReactElement => (
-  <div className="navbar">
-    {console.log('here')}
-    <Header className="title" content="POLARIO" as="h1"/>
-    <Image
-      alt="User Name"
-      size="mini"
-      avatar
-      src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-    />
-  </div>
+  <Sticky>
+    <div className="navbar">
+      {console.log('here')}
+      <Header className="title" content="POLARIO" as="h1" />
+      <Image
+        alt="User Name"
+        size="mini"
+        avatar
+        src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+      />
+    </div>
+  </Sticky>
 );
 
 export default Navbar;
