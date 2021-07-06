048288aed8fb76980823ae305a0fee492596ef8a
Nick King <nrking0@gmail.com>
2021-07-05 19:16:56 -0500

    Worked on stylying and adding info to about page

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -6,23 +6,28 @@ import './style.scss';
 
 
 const About = (): ReactElement => (
-        <>
-            <Navbar />
-            <div className="about-main">
-                <div className="text">
+    <>
+        <Navbar />
+        <div className="about-main">
+            <div className="text">
                 <h1>Take Control of the Information you see</h1>
                 <p>With Polario you can ensure you know the backstory
                     of anything you read on the internet.
                 </p>
                 <Button href="">Download the Extension</Button>
-                <Button href="" color="blue">Learn More</Button>
-                </div>
-
-                <div className="image">
-                    <p>Place-Holder for image of extension</p>
-                </div>
+                <Button onClick={() => window.scroll({ top: window.innerHeight, behavior: 'smooth' })} color="blue">Learn More</Button>
             </div>
-        </>
-    );
+
+            <div className="image">
+                <p>Place-Holder for image of extension</p>
+            </div>
+        </div>
+
+        <div className="about-below">
+            <h1>Information on the Internet can be confusing, we make it simple!</h1>
+            <p>With polario</p>
+        </div>
+    </>
+)
 
 export default About;
